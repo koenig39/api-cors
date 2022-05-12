5e78147b5f6c2bc8f70ee3e8d850d2cb79424cd7
unknown <apotapov@sirostechnologies.com>
2022-05-12 10:51:27 -0600

    fix 2

--- api.py
+++ api.py
@@ -5,10 +5,17 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 api = Api(app)
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
 
 class TestApi(Resource):
     def get(self):
