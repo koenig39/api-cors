41817d7f4ece850c644f7afda78977fd1bb2ee11
unknown <apotapov@sirostechnologies.com>
2022-05-12 09:36:39 -0600

    cors added

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
 from distutils.log import debug
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class TestApi(Resource):
